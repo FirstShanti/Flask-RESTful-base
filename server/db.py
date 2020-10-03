97752be3b2b990cbe439b44d0c99160ec56e63e0
FirstShanti <steppe.alone@gmail.com>
2020-10-03 17:01:19 +0300

    remove db.py

--- server/db.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session
-from sqlalchemy.orm import sessionmaker
-from settings import DB_URI
-
-Session = sessionmaker(autocommit=False,
-                       autoflush=False,
-                       bind=create_engine(DB_URI))
-session = scoped_session(Session)
